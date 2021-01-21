34b92ee016e33bc22f1efe53aad02f91baaec60f
Lo Hedengran <lohedengran@gmail.com>
2021-01-21 14:27:21 +0100

    Initial commit

---
+++ MerryMethodsProject/MethodBench.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MerryMethodsProject
+{
+    class MethodBench
+    {
+        public void FirstMethod()
+        {
+            Console.WriteLine("Merry Methods");
+        }
+
+        public void SecondMethod(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public void ThirdMethod(string input, bool uppercase)
+        {
+            if (uppercase)
+            {
+                input = input.ToUpper();
+            }
+            else
+            {
+                input = input.ToLower();
+            }
+            SecondMethod(input);
+        }
+
+        public bool FourthMethod()
+        {
+            Console.WriteLine("Would you like to shout or whisper? Press S to shout or W to whisper.");
+           
+            
+            var keyInput = Console.ReadKey(true);
+
+            if (keyInput.Key == ConsoleKey.S)
+            {
+                return true;
+            }
+            else if (keyInput.Key == ConsoleKey.W)
+            {
+                return false;
+            }
+            else
+                return false;
+                
+        }
+    }
+}
---
+++ MerryMethodsProject/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MerryMethodsProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodBench methodBench = new MethodBench();
+            int i = 0;
+            do
+            {
+                methodBench.FirstMethod();
+                i++;
+            } while (i < 3);
+
+
+            methodBench.SecondMethod("C# Now and Forever!");
+            Console.WriteLine("Input a string: ");
+            string input = Console.ReadLine();
+            methodBench.SecondMethod(input);
+
+
+            methodBench.ThirdMethod(input, Shout());
+
+            
+        }
+
+        private static bool Shout()
+        {
+            Console.WriteLine("Do you want to whisper or shout?");
+            ConsoleKeyInfo keyinfo = Console.ReadKey(true);
+            bool shout = keyinfo.Key == ConsoleKey.S;
+            return shout;
+        }
+    }
+}
